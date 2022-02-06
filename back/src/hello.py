5acef199b72162eb0b06d3209bd934b2acc1b81e
joe.wright <joe@joejag.com>
2022-02-06 21:50:10 +0000

    formatting

--- back/src/hello.py
+++ back/src/hello.py
@@ -6,18 +6,20 @@ import os
 
 dynamodb = boto3.resource('dynamodb')
 
+
 def hello(token):
     table = dynamodb.Table('weight')
     table.put_item(
-       Item={
+        Item={
             'id': 'joe',
             'creds': json.dumps(token)
         }
     )
 
+
 def lambda_handler(event, context):
     client = login_to_fitbit()
-    d_from = datetime.datetime.strptime('2022-01-30', "%Y-%m-%d")
+    d_from = datetime.datetime.strptime('2022-01-31', "%Y-%m-%d")
     response = fetch(client, d_from)
 
     return {
@@ -31,20 +33,23 @@ def lambda_handler(event, context):
         'body': json.dumps(response)
     }
 
+
 def login_to_fitbit():
     table = dynamodb.Table('weight')
     response = table.get_item(Key={'id': 'joe'})
     creds = json.loads(response['Item']['creds'])
     client = fitbit.Fitbit(client_id="22BJQW",
-                                      client_secret="516c12387e20ea5f5d2516d06a44bdd8",
-                                      refresh_token=creds['refresh_token'],
-                                      access_token=creds['access_token'],
-                                      refresh_cb=hello)
+                           client_secret="516c12387e20ea5f5d2516d06a44bdd8",
+                           refresh_token=creds['refresh_token'],
+                           access_token=creds['access_token'],
+                           refresh_cb=hello)
     client.client.refresh_token()
     return client
 
+
 def lbs_to_kgs(lbs):
     return lbs * 0.453592
+
 
 def fw_to_w(item):
     date = item['date']
@@ -58,19 +63,21 @@ def fw_to_w(item):
         fat = 0
     return {"dateTime": date, "total": float(weight), "lean": float(lean), "fat": float(fat)}
 
+
 def fetch_weight(client, d_from, d_to):
     weights = client.get_bodyweight(base_date=d_from, end_date=d_to)["weight"]
 
     a = []
     for item in weights:
-      a.append(fw_to_w(item))
+        a.append(fw_to_w(item))
 
     return a
 
 
 def fetch_calories(client, d_from, d_to):
-    calories = client.time_series( resource="foods/log/caloriesIn", base_date=d_from, end_date=d_to)
-    
+    calories = client.time_series(
+        resource="foods/log/caloriesIn", base_date=d_from, end_date=d_to)
+
     count = []
     for item in calories["foods-log-caloriesIn"]:
         count.append(item)
@@ -89,6 +96,7 @@ def fetch_exercise(client, d_from, d_to):
     for item in exercises["activities-log-tracker-activityCalories"]:
         count.append(item)
     return count
+
 
 def fetch(client, d_from):
     d_to = d_from + datetime.timedelta(days=4 * 7 - 1)
@@ -118,7 +126,7 @@ def fetch(client, d_from):
         current = merged.get(dt, {})
         current['c'] = calories
         merged[dt] = current
-    
+
     result = []
     for dateTime in merged:
         day = merged[dateTime]
@@ -138,6 +146,7 @@ def fetch(client, d_from):
         })
 
     return result
+
 
 def cachable(name, fn):
     if os.path.isfile(name):
