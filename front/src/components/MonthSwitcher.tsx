a512761493400ddaf370853e34f97730213dd96f
joe.wright <joe@joejag.com>
2025-05-10 17:08:08 +0100

    refactor

--- front/src/components/MonthSwitcher.tsx
+++ front/src/components/MonthSwitcher.tsx
@@ -3,6 +3,13 @@ import React from 'react'
 export const MonthSwitcher = ({ onChange }: { onChange: Function }) => {
   const [firstDayOfTheMonth, setFirstDayOfTheMonth] = React.useState(new Date(new Date().getFullYear(), new Date().getMonth(), 1))
 
+  const previousMonth = new Date(firstDayOfTheMonth.getFullYear(), firstDayOfTheMonth.getMonth() - 1, 1).toLocaleString('default', {
+    month: 'long',
+  })
+  const nextMonth = new Date(firstDayOfTheMonth.getFullYear(), firstDayOfTheMonth.getMonth() + 1, 1).toLocaleString('default', {
+    month: 'long',
+  })
+
   const goBackAMonth = () => {
     setFirstDayOfTheMonth(new Date(firstDayOfTheMonth.getFullYear(), firstDayOfTheMonth.getMonth() - 1, 1))
     onChange(new Date(firstDayOfTheMonth.getFullYear(), firstDayOfTheMonth.getMonth() - 1, 1))
@@ -16,15 +23,11 @@ export const MonthSwitcher = ({ onChange }: { onChange: Function }) => {
   return (
     <div className="navigation-buttons">
       <button className="nav-button prev" onClick={goBackAMonth}>
-        {new Date(firstDayOfTheMonth.getFullYear(), firstDayOfTheMonth.getMonth() - 1, 1).toLocaleString('default', {
-          month: 'long',
-        })}
+        {previousMonth}
       </button>
       {new Date(firstDayOfTheMonth.getFullYear(), firstDayOfTheMonth.getMonth() + 1, 1) < new Date() && (
         <button className="nav-button next" onClick={goForwardAMonth}>
-          {new Date(firstDayOfTheMonth.getFullYear(), firstDayOfTheMonth.getMonth() + 1, 1).toLocaleString('default', {
-            month: 'long',
-          })}
+          {nextMonth}
         </button>
       )}
     </div>
