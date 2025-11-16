75119a7a3585678b9507b0fc157084341bd72e99
Joe Wright <joe@joejag.com>
2025-11-16 13:45:48 +0000

    Update config.ts

--- front/src/biz/config.ts
+++ front/src/biz/config.ts
@@ -1,7 +1,7 @@
 export const FAT_LOSS_GOAL = 2 // kgs
 export const FAT_LOSS_TOTAL_GOAL = 16
 
-export const NEXT_BIG_EVENT = 'See Sam'
-export const NEXT_BIG_EVENT_DATE = new Date(2025, 11 - 1, 7)
+export const NEXT_BIG_EVENT = 'Vegas'
+export const NEXT_BIG_EVENT_DATE = new Date(2025, 11 - 1, 28)
 
 export const START_DATE: Date = new Date('2024-12-15')
